730265821987b2845b783362d72ba4fa0087b93e
Jonathan Hampton <hamptonjc221@gmail.com>
2020-11-30 18:22:14 -0500

    finished fixing dimension issues in residual blocks
    Now number of parameters is correct

--- ResNet50.py
+++ ResNet50.py
@@ -50,52 +50,49 @@ class ResNet50(tf.keras.Model):
                                             throughout the resnet architechture.
 
         """
-        def __init__(self, name:str, input_shape:tuple, conv1_2_filters: int, conv3_filters: int,
-                        conv1_stride: int=1, conv2_3_stride: int=1,
-                        use_bias: bool=False, skip_connection_stride: int=(1,1)):
+        def __init__(self, name:str, input_shape:tuple,
+                        conv1_filters:int, conv2_filters:int, conv3_filters: int,
+                        conv1_stride:int, conv2_stride:int, conv3_stride:int,
+                        skip_conv:bool=False, skip_pool:bool=False, skip_connection_stride: int=(1,1)):
             
-            super().__init__()
-            
+            super().__init__() 
             self._name = name
             
             # Layers
             self.input_layer = kl.InputLayer(input_shape)
-            
             self.preact_bn = kl.BatchNormalization(epsilon=1.001e-05, name=name+'_preact_bn')
-            
             self.preact_relu = kl.Activation('relu', name=name+'_preact_relu')
-
-            self.conv1 = kl.Conv2D(filters=conv1_2_filters, kernel_size=(1,1), strides=conv1_stride,
-                                    use_bias=use_bias, activation='linear', name=name+'_conv1')
-            
+            self.conv1 = kl.Conv2D(filters=conv1_filters, kernel_size=(1,1), strides=conv1_stride,
+                                    use_bias=False, activation='linear', name=name+'_conv1')
             self.bn1 = kl.BatchNormalization(epsilon=1.001e-05, name=name+'_bn1')
-
             self.relu1 = kl.Activation('relu', name=name+'_relu1')
-
             self.pad1 = kl.ZeroPadding2D(padding=((1,1),(1,1)), name=name+'_pad1')
-
-            self.conv2 = kl.Conv2D(filters=conv1_2_filters, kernel_size=(3,3), strides=conv2_3_stride,
-                                    use_bias=use_bias, activation='linear', name=name+'_conv2')
-
+            self.conv2 = kl.Conv2D(filters=conv2_filters, kernel_size=(3,3), strides=conv2_stride,
+                                    use_bias=False, activation='linear', name=name+'_conv2')
             self.bn2 = kl.BatchNormalization(epsilon=1.001e-05, name=name+'_bn2')
-
             self.relu2 = kl.Activation('relu', name=name+'_relu2')
+            self.skip_conv = None
+            self.skip_pool = None
+            if skip_conv:
+                self.skip_conv = kl.Conv2D(filters=conv3_filters, kernel_size=(1,1),
+                                            strides=skip_connection_stride,
+                                            name=name+'_skip_conv')
+            if skip_pool:
+                self.skip_pool = kl.MaxPooling2D(pool_size=(1,1), strides=(2,2),
+                                                    padding='valid', name=name+"_skip_pool")
 
-            self.skip_conv = kl.Conv2D(filters=conv3_filters, kernel_size=(1,1),
-                                        strides=skip_connection_stride,
-                                        name=name+'_skip_conv')
-
-            self.conv3 = kl.Conv2D(filters=conv3_filters, kernel_size=(1,1), strides=conv2_3_stride,
-                                    activation='linear', name=name+'_conv3')
+            self.conv3 = kl.Conv2D(filters=conv3_filters, kernel_size=(1,1),
+                                    strides=conv3_stride,activation='linear', name=name+'_conv3')
 
             # Build
             self.build(input_shape)
 
         def call(self, input_tensor: tf.Tensor,
                     training: bool=None) -> tf.Tensor:
-            x = self.input_layer(input_tensor)
-            x = self.preact_bn(x)
+            input_tensor = self.input_layer(input_tensor)
+            x = self.preact_bn(input_tensor)
             x = self.preact_relu(x)
+            skip_connection = x
             x = self.conv1(x)
             x = self.bn1(x)
             x = self.relu1(x)
@@ -103,8 +100,17 @@ class ResNet50(tf.keras.Model):
             x = self.conv2(x)
             x = self.bn2(x)
             x = self.relu2(x)
-            skip_connection = self.skip_conv(input_tensor)
             x = self.conv3(x)
+
+            if self.skip_conv is not None:
+                skip_connection = self.skip_conv(skip_connection)
+            
+            elif self.skip_pool is not None:
+                skip_connection = self.skip_pool(input_tensor)
+
+            else:
+                skip_connection = input_tensor
+
             output = x + skip_connection
             return output
 
@@ -145,75 +151,90 @@ class ResNet50(tf.keras.Model):
                                         padding='valid',name='pool1_pool')
 
         # Stage II
-        self.conv2_1 = self.ResidualBlock(input_shape=(None,56,56,64), conv1_2_filters=64,
-                                            conv3_filters=256, conv1_stride=(1,1),
-                                            conv2_3_stride=(1,1), name='conv2_block1')
+        self.conv2_1 = self.ResidualBlock(name='conv2_block1', input_shape=(None,56,56,64),
+                                            conv1_filters=64, conv2_filters=64, conv3_filters=256,
+                                            conv1_stride=(1,1), conv2_stride=(1,1), conv3_stride=(1,1),
+                                            skip_conv=True, skip_connection_stride=(1,1))
 
-        self.conv2_2 = self.ResidualBlock(input_shape=(None,56,56,256), conv1_2_filters=64,
-                                            conv3_filters=256, conv1_stride=(1,1),
-                                            conv2_3_stride=(1,1), name='conv2_block2')
+        self.conv2_2 = self.ResidualBlock(name='conv2_block2', input_shape=(None,56,56,256),
+                                            conv1_filters=64, conv2_filters=64, conv3_filters=256,
+                                            conv1_stride=(1,1),conv2_stride=(1,1), conv3_stride=(1,1))
 
-        self.conv2_3 = self.ResidualBlock(input_shape=(None,56,56,256), conv1_2_filters=64,
-                                            conv3_filters=256, conv1_stride=(1,1),
-                                            conv2_3_stride=(1,1), name='conv2_block3')
-
+        self.conv2_3 = self.ResidualBlock(name='conv2_block3', input_shape=(None,56,56,256),
+                                            conv1_filters=64, conv2_filters=64, conv3_filters=256,
+                                            conv1_stride=(1,1), conv2_stride=(2,2), conv3_stride=(1,1),
+                                            skip_pool=True)
         # Stage III
-        self.conv3_1 = self.ResidualBlock(input_shape=(None,28,28,256), conv1_2_filters=128,
-                                            conv3_filters=512, conv1_stride=(2,2),
-                                            conv2_3_stride=(1,1), skip_connection_stride=2,
-                                            name='conv3_block1')
+        self.conv3_1 = self.ResidualBlock(name='conv3_block1', input_shape=(None,28,28,256),
+                                            conv1_filters=128, conv2_filters=128, conv3_filters=512,
+                                            conv1_stride=(1,1), conv2_stride=(1,1), conv3_stride=(1,1),
+                                            skip_conv=True, skip_connection_stride=(1,1))
+                                            
 
-        self.conv3_2 = self.ResidualBlock(input_shape=(None,28,28,512), conv1_2_filters=128,
-                                            conv3_filters=512,conv1_stride=(1,1), conv2_3_stride=(1,1),
-                                            name='conv3_block2')
+        self.conv3_2 = self.ResidualBlock(name='conv3_block2', input_shape=(None,28,28,512),
+                                            conv1_filters=128, conv2_filters=128, conv3_filters=512,
+                                            conv1_stride=(1,1), conv2_stride=(1,1), conv3_stride=(1,1))
+                                            
 
-        self.conv3_3 = self.ResidualBlock(input_shape=(None,28,28,512), conv1_2_filters=128,
-                                            conv3_filters=512, conv1_stride=(1,1), conv2_3_stride=(1,1),
-                                            name='conv3_block3')
+        self.conv3_3 = self.ResidualBlock(name='conv3_block3', input_shape=(None,28,28,512),
+                                            conv1_filters=128, conv2_filters=128, conv3_filters=512,
+                                            conv1_stride=(1,1), conv2_stride=(1,1), conv3_stride=(1,1))
+                                            
 
-        self.conv3_4 = self.ResidualBlock(input_shape=(None,28,28,512), conv1_2_filters=128,
-                                            conv3_filters=512, conv1_stride=(1,1), conv2_3_stride=(1,1),
-                                            name='conv3_block4')
+        self.conv3_4 = self.ResidualBlock(name='conv3_block4', input_shape=(None,28,28,512),
+                                            conv1_filters=128, conv2_filters=128, conv3_filters=512,
+                                            conv1_stride=(1,1), conv2_stride=(2,2),conv3_stride=(1,1),
+                                            skip_pool=True)
 
         # Stage IV
-        self.conv4_1 = self.ResidualBlock(input_shape=(None,14,14,512), conv1_2_filters=256,
-                                            conv3_filters=1024, conv1_stride=(2,2),
-                                            conv2_3_stride=(1,1), skip_connection_stride=(2,2),
-                                            name='conv4_block1')
+        self.conv4_1 = self.ResidualBlock(name='conv4_block1', input_shape=(None,14,14,512),
+                                            conv1_filters=256, conv2_filters=256, conv3_filters=1024,
+                                            conv1_stride=(1,1), conv2_stride=(1,1), conv3_stride=(1,1),
+                                            skip_conv=True, skip_connection_stride=(1,1))
+                                            
 
-        self.conv4_2 = self.ResidualBlock(input_shape=(None,14,14,1024), conv1_2_filters=256,
-                                            conv3_filters=1024, conv1_stride=(1,1), conv2_3_stride=(1,1),
-                                            name='conv4_block2')
+        self.conv4_2 = self.ResidualBlock(name='conv4_block2', input_shape=(None,14,14,1024),
+                                            conv1_filters=256, conv2_filters=256, conv3_filters=1024,
+                                            conv1_stride=(1,1), conv2_stride=(1,1), conv3_stride=(1,1))
+                                            
 
-        self.conv4_3 = self.ResidualBlock(input_shape=(None,14,14,1024), conv1_2_filters=256,
-                                            conv3_filters=1024, conv1_stride=(1,1), conv2_3_stride=(1,1),
-                                            name='conv4_block3')
+        self.conv4_3 = self.ResidualBlock(name='conv4_block3', input_shape=(None,14,14,1024),
+                                            conv1_filters=256, conv2_filters=256, conv3_filters=1024,
+                                            conv1_stride=(1,1), conv2_stride=(1,1), conv3_stride=(1,1))
+                                            
 
-        self.conv4_4 = self.ResidualBlock(input_shape=(None,14,14,1024), conv1_2_filters=256,
-                                            conv3_filters=1024, conv1_stride=(1,1), conv2_3_stride=(1,1),
-                                            name='conv4_block4')
+        self.conv4_4 = self.ResidualBlock(name='conv4_block4', input_shape=(None,14,14,1024),
+                                            conv1_filters=256, conv2_filters=256, conv3_filters=1024,
+                                            conv1_stride=(1,1), conv2_stride=(1,1), conv3_stride=(1,1))
+                                            
 
-        self.conv4_5 = self.ResidualBlock(input_shape=(None,14,14,1024), conv1_2_filters=256,
-                                            conv3_filters=1024, conv1_stride=(1,1), conv2_3_stride=(1,1),
-                                            name='conv4_block5')
+        self.conv4_5 = self.ResidualBlock(name='conv4_block5', input_shape=(None,14,14,1024),
+                                            conv1_filters=256, conv2_filters=256, conv3_filters=1024,
+                                            conv1_stride=(1,1), conv2_stride=(1,1), conv3_stride=(1,1))
+                                            
 
-        self.conv4_6 = self.ResidualBlock(input_shape=(None,14,14,1024),conv1_2_filters=256,
-                                            conv3_filters=1024, conv1_stride=(1,1), conv2_3_stride=(1,1),
-                                            name='conv4_block6')
+        self.conv4_6 = self.ResidualBlock(name='conv4_block6', input_shape=(None,14,14,1024),
+                                            conv1_filters=256, conv2_filters=256, conv3_filters=1024,
+                                            conv1_stride=(1,1), conv2_stride=(2,2), conv3_stride=(1,1),
+                                            skip_pool=True)
+                                            
 
         # Stage V
-        self.conv5_1 = self.ResidualBlock(input_shape=(None,7,7,1024), conv1_2_filters=512,
-                                            conv3_filters=2048, conv1_stride=(2,2),
-                                            conv2_3_stride=(1,1), skip_connection_stride=(2,2),
-                                            name='conv5_block1')
+        self.conv5_1 = self.ResidualBlock(name='conv5_block1', input_shape=(None,7,7,1024),
+                                            conv1_filters=512, conv2_filters=512, conv3_filters=2048,
+                                            conv1_stride=(1,1), conv2_stride=(1,1), conv3_stride=(1,1),
+                                            skip_conv=True, skip_connection_stride=(1,1))
+                                            
 
-        self.conv5_2 = self.ResidualBlock(input_shape=(None,7,7,2048), conv1_2_filters=512,
-                                            conv3_filters=2048, conv1_stride=(1,1), conv2_3_stride=(1,1),
-                                            name='conv5_block2')
+        self.conv5_2 = self.ResidualBlock(name='conv5_block2', input_shape=(None,7,7,2048),
+                                            conv1_filters=512, conv2_filters=512, conv3_filters=2048,
+                                            conv1_stride=(1,1), conv2_stride=(1,1), conv3_stride=(1,1))
+                                            
 
-        self.conv5_3 = self.ResidualBlock(input_shape=(None,7,7,2048), conv1_2_filters=512,
-                                            conv3_filters=2048, conv1_stride=(1,1), conv2_3_stride=(1,1),
-                                            name='conv5_block3')
+        self.conv5_3 = self.ResidualBlock(name='conv5_block3', input_shape=(None,7,7,2048),
+                                            conv1_filters=512, conv2_filters=512, conv3_filters=2048,
+                                            conv1_stride=(1,1), conv2_stride=(1,1), conv3_stride=(1,1))
+                                            
 
         # Post Stage
         self.post_bn = kl.BatchNormalization(epsilon=1.001e-05, name='post_bn')
